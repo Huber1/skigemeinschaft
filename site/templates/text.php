eab6feac5a1be9c70eb061e8900eddb45e3f5357
Moritz Huber <2nd@moritzhuber.de>
2025-08-01 16:27:19 +0200

    textseite

---
+++ site/templates/text.php
@@ -0,0 +1,21 @@
+<?php
+
+use Kirby\Panel\Page;
+
+/**
+ * @var Page $page
+ */
+
+?>
+<?php snippet('header') ?>
+
+<div class="py-6">
+  <h1 class="text-center text-3xl"><?= $page->title()->esc() ?></h1>
+</div>
+
+<!-- Content -->
+<article class="prose">
+  <?= $page->text()->kt() ?>
+</article>
+
+<?php snippet('footer') ?>
