193bb233c08260b94b6b5ba26c385a0022125cb2
Moritz Huber <2nd@moritzhuber.de>
2025-08-13 20:08:32 +0200

    widget url

--- site/templates/angebot.php
+++ site/templates/angebot.php
@@ -27,7 +27,7 @@ use Kirby\Cms\Page;
     <div class="prose max-w-none">
       <?= $page->content()->text()->kirbytext() ?>
     </div>
-    <div class="mt-4">
+    <div class="mt-8">
       <table>
         <tr>
           <td class="font-bold align-top pr-4">Termin</td>
@@ -47,6 +47,24 @@ use Kirby\Cms\Page;
         <?php endif ?>
       </table>
     </div>
+    <?php if ($page->content()->widget() != null): ?>
+      <div class="mt-12">
+        <div class="flex justify-between items-center mb-4">
+          <h1 class="text-2xl">Anmeldung</h1>
+          <a href="#" class="flex items-center gap-1">
+            In neuem Tab öffnen
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+              <path
+                d="M10 6V8H5V19H16V14H18V20C18 20.5523 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V7C3 6.44772 3.44772 6 4 6H10ZM21 3V11H19L18.9999 6.413L11.2071 14.2071L9.79289 12.7929L17.5849 5H13V3H21Z"></path>
+            </svg>
+          </a>
+        </div>
+        <iframe
+          src="<?= $page->content()->widget() ?>"
+          class="w-full h-144"
+        ></iframe>
+      </div>
+    <?php endif ?>
   </div>
   <div>
     <?php if ($page->content()->cover()->toFile() != null): ?>
