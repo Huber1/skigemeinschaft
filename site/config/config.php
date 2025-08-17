b9a39d2ef3c6435743c9856c1ccc4c58e3fe2002
Moritz Huber <2nd@moritzhuber.de>
2025-08-18 00:48:42 +0200

    sitemap

--- site/config/config.php
+++ site/config/config.php
@@ -3,11 +3,34 @@
 /**
  * The config file is optional. It accepts a return array with config options
  * Note: Never include more than one return statement, all options go within this single return array
- * In this example, we set debugging to true, so that errors are displayed onscreen. 
+ * In this example, we set debugging to true, so that errors are displayed onscreen.
  * This setting must be set to false in production.
  * All config options: https://getkirby.com/docs/reference/system/options
  */
 return [
     'debug' => true,
     'yaml.handler' => 'symfony', // already makes use of the more modern Symfony YAML parser: https://getkirby.com/docs/reference/system/options/yaml (will become the default in a future Kirby version)
+    'routes' => [
+        [
+            'pattern' => 'sitemap.xml',
+            'action' => function () {
+                $pages = site()->pages()->index();
+
+                // fetch the pages to ignore from the config settings,
+                // if nothing is set, we ignore the error page
+                $ignore = kirby()->option('sitemap.ignore', ['error']);
+
+                $content = snippet('sitemap', compact('pages', 'ignore'), true);
+
+                // return response with correct header type
+                return new Kirby\Cms\Response($content, 'application/xml');
+            },
+        ],
+        [
+            'pattern' => 'sitemap',
+            'action' => function () {
+                return go('sitemap.xml', 301);
+            },
+        ],
+    ],
 ];
