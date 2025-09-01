0bf6e22a0d9ade146b5c96a8bb8cb1f1db94fc3c
Moritz Huber <2nd@moritzhuber.de>
2025-09-01 13:34:25 +0200

    alter range

--- site/templates/angebote.php
+++ site/templates/angebote.php
@@ -1,10 +1,21 @@
 <?php
 
-use Kirby\Cms\Page;
-
 /**
  * @var Page $page
  */
+
+use Kirby\Cms\Page;
+
+function format_age(int $minAge, int $maxAge): string|null
+{
+  if ($minAge === 0 && $maxAge === 0) return null;
+
+  if ($minAge !== 0 && $maxAge === 0) return "Ab $minAge Jahren";
+
+  if ($minAge === 0 && $maxAge !== 0) return "Bis $maxAge Jahren";
+
+  return "Von $minAge bis $maxAge Jahren";
+}
 
 ?>
 <?php snippet('header') ?>
@@ -22,13 +33,23 @@ use Kirby\Cms\Page;
           class="flex-2/5 bg-cover rounded-lg"
           style="background-image: url('<?= $child->cover()->toFile()->url() ?>')">
         </div>
-        <div class="flex-3/5 flex flex-col gap-4">
+        <div class="flex-3/5 flex flex-col gap-2">
           <h2 class="text-xl font-medium group-hover:text-salmon">
             <?= $child->title() ?>
           </h2>
-          <div class="text-primary">
-            <?= $child->termin() ?>
+          <div class="text-primary flex items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
+              <path
+                d="M9 1V3H15V1H17V3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H7V1H9ZM20 11H4V19H20V11ZM8 13V15H6V13H8ZM13 13V15H11V13H13ZM18 13V15H16V13H18ZM7 5H4V9H20V5H17V7H15V5H9V7H7V5Z"></path>
+            </svg><?= $child->termin() ?>
           </div>
+          <?php
+          $alter = format_age($child->minAlter()->toInt(), $child->maxAlter()->toInt());
+          if ($alter != null): ?>
+            <div class="text-primary">
+              <?= $alter ?>
+            </div>
+          <?php endif ?>
           <div class="line-clamp-3 text-gray-600">
             <?= $child->text()->kirbytext() ?>
           </div>
