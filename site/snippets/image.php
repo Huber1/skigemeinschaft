5bdb10b0c1c5e36b0f10dc6095510d16485a58ab
Moritz Huber <2nd@moritzhuber.de>
2025-10-06 22:00:14 +0200

    home picture fix

--- site/snippets/image.php
+++ site/snippets/image.php
@@ -1,26 +1,42 @@
 <?php
-/** @var \Kirby\Cms\File $image */
+/** @var File $image
+ * @var string|null $position
+ */
 
-$sizes = "(min-width: 1200px) 25vw,
-        (min-width: 900px) 33vw,
-        (min-width: 600px) 50vw,
-        100vw";
+use Kirby\Cms\File;
+
+$pos = match ($position ?? null) {
+  'left' => 'object-left',
+  'right' => 'object-right',
+  'top' => 'object-top',
+  'bottom' => 'object-bottom',
+  default => 'object-center',
+};
+
+$class = "rounded-lg absolute w-full h-full object-cover $pos";
+
+$sizes = "(min-width: 1200px) 50vw,
+        (min-width: 900px) 66vw,
+        (min-width: 600px) 100vw,
+        200vw";
 ?>
 
 <div class="w-full h-full overflow-hidden relative">
-  <picture class="absolute w-full h-full object-cover top-0 left-0">
+  <picture>
     <source
+      class="<?= $class ?>"
       srcset="<?= $image->srcset('avif') ?>"
       sizes="<?= $sizes ?>"
       type="image/avif"
     >
     <source
+      class="<?= $class ?>"
       srcset="<?= $image->srcset('webp') ?>"
       sizes="<?= $sizes ?>"
       type="image/webp"
     >
     <img
-      class="rounded-lg absolute w-full h-full object-cover top-0 left-0"
+      class="<?= $class ?>"
       alt="<?= $image->alt() ?>"
       src="<?= $image->resize(300)->url() ?>"
       srcset="<?= $image->srcset() ?>"
