4be8755ebb8060265eac7c5e11906dfb5a089095
Moritz Huber <2nd@moritzhuber.de>
2025-10-27 22:32:12 +0100

    no exception when background image missing

--- site/templates/home.php
+++ site/templates/home.php
@@ -20,7 +20,7 @@ $backgroundFile = $page->content()->background()->toFile();
 
 <!-- Hero section -->
 <div class="h-128">
-  <?php snippet('bgImage', ['image' => $backgroundFile, 'position' => 'bottom']) ?>
+  <?php if($backgroundFile) snippet('bgImage', ['image' => $backgroundFile, 'position' => 'bottom']) ?>
 </div>
 
 <div class="px-8 mt-16">
