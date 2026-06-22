322d262542b8f89034db1c48330d907c5765b27e
Moritz Huber <2nd@moritzhuber.de>
2026-06-22 22:52:36 +0200

    Nix dev env

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Nix dev environment for php and nodejs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          php85
+          php85Packages.composer
+          nodejs
+          yarn-berry
+        ];
+      };
+
+    };
+}
